644a88d66c413a53a47a4d245990e414fe92464c
Andrew Gordon <47512060+gxrdon@users.noreply.github.com>
2020-04-29 11:28:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -231,6 +231,6 @@ app.post("/sendMail", function(req, res){
   res.redirect('/listing/' + currSlug);
 })
 
-http.listen(3000, function() {
+/*http.listen(3000, function() {
     console.log('Listening on port 3000!');
-});
+});*/
